afeee1a5a2b99a8ae3c497331862ace65d8d23e0
Dark4Angel <quakextreme@msn.com>
2024-06-06 04:14:27 -0500

    usersfronted redux

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,20 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './interceptors/token.interceptor';
+import { provideStore } from '@ngrx/store';
+import { usersReducer } from './store/users.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { UsersEffects } from './store/users.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideStore({
+        users: usersReducer
+    }),
+    provideEffects(UsersEffects)
+]
 };
